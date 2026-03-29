2420df0650318f1312220bb2f19208d53deb4bae
DulajGunarathna2004 <dulajgunarathna25@gmail.com>
2026-03-29 12:11:12 +1100

    package updates

--- packages/square_turtle_pkg/src/square_turtle.py
+++ packages/square_turtle_pkg/src/square_turtle.py
@@ -1,49 +1,31 @@
-
+#!/usr/bin/env python3
 
 import rospy
 from geometry_msgs.msg import Twist
 
-def move_square():
-    rospy.init_node('square_turtle_node', anonymous=True)
+def draw_square():
+    rospy.init_node('square_turtle', anonymous=True)
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
-    rate = rospy.Rate(10)  # 10 times per second
-    vel = Twist()
-
-    rospy.sleep(1)
+    move = Twist()
+    rate = rospy.Rate(1)  # 1 loop per second
 
     while not rospy.is_shutdown():
-        # Move forward
-        vel.linear.x = 2.0
-        vel.angular.z = 0.0
-        for _ in range(20):   # move for 2 seconds
-            pub.publish(vel)
-            rate.sleep()
+        for i in range(4):
+            # Move forward
+            move.linear.x = 2.0
+            move.angular.z = 0.0
+            pub.publish(move)
+            rospy.sleep(2)
 
-        # Stop a little
-        vel.linear.x = 0.0
-        vel.angular.z = 0.0
-        for _ in range(5):
-            pub.publish(vel)
-            rate.sleep()
-
-        # Turn left 90 degrees
-        vel.linear.x = 0.0
-        vel.angular.z = 1.57
-        for _ in range(10):   # turn for about 1 second
-            pub.publish(vel)
-            rate.sleep()
-
-        # Stop a little
-        vel.linear.x = 0.0
-        vel.angular.z = 0.0
-        for _ in range(5):
-            pub.publish(vel)
-            rate.sleep()
+            # Turn 90 degrees
+            move.linear.x = 0.0
+            move.angular.z = 1.57
+            pub.publish(move)
+            rospy.sleep(1)
 
 if __name__ == '__main__':
     try:
-        rospy.loginfo("Turtles are great at drawing squares!")
-        move_square()
+        draw_square()
     except rospy.ROSInterruptException:
         pass
